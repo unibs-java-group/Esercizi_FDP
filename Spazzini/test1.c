c5501649d5d5f00f732be0d8c0fac75b6b3db59d
SpaMichUnibs <81171562+SpaMichUnibs@users.noreply.github.com>
2021-03-29 19:58:26 +0200

    Update and rename test1.java to test1.c

--- Spazzini/test1.c
+++ Spazzini/test1.c
@@ -7,7 +7,7 @@ int i;
 char discorso[80];
   
 for(i=0; (discorso[i]=frase[i])!='\0'; i++){
-  printf(" originale: %s \n copia: %s \n", frase, discorso)
+  printf("Originale: %s \n Copua: %s \n", frase, discorso)
 }
   
 system("pause");

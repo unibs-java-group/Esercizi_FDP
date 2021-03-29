d97273ce7d61650c5b5e56ee2461aaeb7c569be0
SpaMichUnibs <81171562+SpaMichUnibs@users.noreply.github.com>
2021-03-29 19:58:09 +0200

    Create test1.java

---
+++ Spazzini/test1.java
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+char frase[] = "Analisi, requisiti ";
+
+int main(void){
+int i;
+char discorso[80];
+  
+for(i=0; (discorso[i]=frase[i])!='\0'; i++){
+  printf(" originale: %s \n copia: %s \n", frase, discorso)
+}
+  
+system("pause");
+return 0;
+}

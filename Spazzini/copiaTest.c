05b0c327781f5f47d5f37d21b01df3efc623bdc1
SpaMichUnibs <81171562+SpaMichUnibs@users.noreply.github.com>
2021-03-29 20:00:52 +0200

    Update copiaTest.c

--- Spazzini/copiaTest.c
+++ Spazzini/copiaTest.c
@@ -3,13 +3,13 @@
 char frase[] = "Analisi, requisiti ";
 
 int main(void){
-int i;
-char discorso[80];
+  int i;
+  char discorso[80];
   
-for(i=0; (discorso[i]=frase[i])!='\0'; i++){
-  printf("Originale: %s \n Copua: %s \n", frase, discorso)
-}
+  for(i=0; (discorso[i]=frase[i])!='\0'; i++){
+    printf("Originale: %s \n Copua: %s \n", frase, discorso)
+  }
   
-system("pause");
-return 0;
+  system("pause");
+  return 0;
 }
